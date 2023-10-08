ff9dc6c97534ccddc43e3a9eb6a6393d2a6b4202
lasklusterer <minh.nn210582@sis.hust.edu.vn>
2023-10-09 00:07:08 +0700

    Added some comments

--- app/src/main/java/com/hust/diceroller/MainActivity.kt
+++ app/src/main/java/com/hust/diceroller/MainActivity.kt
@@ -5,6 +5,9 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity rolls a 6-faced-dice and show result on screen
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,14 +15,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Roll the dice, update `TextView` with the result
+     */
     private fun rollDice() {
+        // Create new 6-faced-dice and roll
         val dice = Dice(6)
         val diceRoll = dice.roll()
+
+        // Update with the roll result
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
